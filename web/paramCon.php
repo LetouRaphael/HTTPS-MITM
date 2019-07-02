07028c51e430a57034688dc064f1f05d051c521a
LetouRaphael <48223543+LetouRaphael@users.noreply.github.com>
2019-07-02 20:36:06 +0200

    Add files via upload

---
+++ web/paramCon.php
@@ -0,0 +1,16 @@
+<?php
+// information sur les parametres de connexion a la base de donn\E9es
+//----------------------------------------------------
+  // Mettre ci-dessous votre login bdd:
+$user="postgres";
+ // Mettre ci-dessous votre mot de passe bdd
+$pass="postgres";
+// Mettre ci-dessous le nom de votre base
+$dbname="projet_letournr";
+// Mettre ci-dessous le nom du host (depend du serveur). Si le serveur web se trouve sur la m\EAme machine que le serveur web, la valeur sera "localhost".
+$lehost="localhost";
+// Mettre ci-dessous le nom du port (depend de la config du serveur). G\E9n\E9ralement 5432.
+$leport="5432";
+
+
+?>
